cd8a43d74786515c4145d99a225912772f27512a
Youri Bijen <youri@bijen-it.nl>
2021-12-04 09:58:24 +0100

    Day 1 - part 1

--- AdventOfCode/Day_01.cs
+++ AdventOfCode/Day_01.cs
@@ -4,15 +4,33 @@ namespace AdventOfCode
 {
     public class Day_01 : BaseDay
     {
-        private readonly string _input;
+        private readonly List<int> _inputNumbers;
 
         public Day_01()
         {
-            _input = File.ReadAllText(InputFilePath);
+            _inputNumbers = File.ReadAllLines(InputFilePath).Select(line => int.Parse(line)).ToList();
         }
 
-        public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+        public override ValueTask<string> Solve_1() => CountIncrementingDepthMeasuresAsync(_inputNumbers);
 
         public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+
+        private ValueTask<string> CountIncrementingDepthMeasuresAsync(List<int> allMeasures)
+            => new(CountIncrementingDepthMeasures(allMeasures).ToString());
+        private int CountIncrementingDepthMeasures(List<int> allMeasures)
+        {
+            var count = 0;
+            var previous = allMeasures.First();
+            foreach(var measure in allMeasures.Skip(1))
+            {
+                if(measure > previous)
+                {
+                    count++;
+                }
+                previous = measure;
+            }
+
+            return count;
+        }
     }
 }
