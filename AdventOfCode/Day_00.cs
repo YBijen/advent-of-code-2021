63e950a093a94ce709ae7b77b15ce66fb93d1949
Youri Bijen <youri@bijen-it.nl>
2021-12-05 11:45:46 +0100

    Added Day template

---
+++ AdventOfCode/Day_00.cs
@@ -0,0 +1,18 @@
+﻿using AoCHelper;
+
+namespace AdventOfCode
+{
+    public class Day_00 : BaseDay
+    {
+        private readonly string _input;
+
+        public Day_00()
+        {
+            _input = File.ReadAllText(InputFilePath);
+        }
+
+        public override ValueTask<string> Solve_1() => new(_input.Length.ToString());
+
+        public override ValueTask<string> Solve_2() => new("Answer for Part 2 will be here...");
+    }
+}
