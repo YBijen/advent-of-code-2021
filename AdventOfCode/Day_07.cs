27c580783e80ecc95eb97096809713691564bba4
Youri Bijen <youri@bijen-it.nl>
2021-12-07 17:07:09 +0100

    Day 7 part 1 refactor

--- AdventOfCode/Day_07.cs
+++ AdventOfCode/Day_07.cs
@@ -10,29 +10,16 @@ namespace AdventOfCode
         {
             _horizontalPositions = File.ReadAllText(InputFilePath).Split(',').Select(value => int.Parse(value)).ToList();
 
-            Console.WriteLine("Median: " + FindMedian(_horizontalPositions));
+            Console.WriteLine("Max: " + _horizontalPositions.Max());
         }
 
         public override ValueTask<string> Solve_1() => new(CalculcateFuelToPosition(_horizontalPositions, FindMedian(_horizontalPositions)).ToString());
 
         public override ValueTask<string> Solve_2() => new("Answer for Part 2 will be here...");
 
-        private int CalculcateFuelToPosition(List<int> horizontalPositions, int targetPosition)
-        {
-            var spentFuel = 0;
-            foreach(var position in horizontalPositions)
-            {
-                if(position > targetPosition)
-                {
-                    spentFuel += (position - targetPosition);
-                }
-                else
-                {
-                    spentFuel += (targetPosition - position);
-                }
-            }
-            return spentFuel;
-        }
+        private int CalculcateFuelToPosition(List<int> horizontalPositions, int targetPosition) => horizontalPositions
+            .Select(position => (position > targetPosition) ? (position - targetPosition) : (targetPosition - position))
+            .Sum();
 
         private int FindMedian(List<int> horizontalPositions)
         {
@@ -40,14 +27,11 @@ namespace AdventOfCode
             currentPositions.Sort();
 
             var centerValueIndex = currentPositions.Count / 2;
-            if(currentPositions.Count % 2 == 0)
-            {
-                return (currentPositions[centerValueIndex] + currentPositions[centerValueIndex - 1]) / 2;
-            }
-            else
-            {
-                return currentPositions[centerValueIndex];
-            }
+            return IsEven(currentPositions.Count)
+                ? (currentPositions[centerValueIndex] + currentPositions[centerValueIndex - 1]) / 2
+                : currentPositions[centerValueIndex];
         }
+
+        private bool IsEven(int value) => value % 2 == 0;
     }
 }
