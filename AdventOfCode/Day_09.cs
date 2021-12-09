dec668bdd6b6ff84f16d031bc1b287a464a79be4
Youri Bijen <youri@bijen-it.nl>
2021-12-09 17:02:02 +0100

    Day 9 - Part 1 small refactor

--- AdventOfCode/Day_09.cs
+++ AdventOfCode/Day_09.cs
@@ -24,46 +24,38 @@ namespace AdventOfCode
             }
         }
 
-        public override ValueTask<string> Solve_1() => new(CalculateRiskLevel(_heightmap).ToString());
+        public override ValueTask<string> Solve_1() => new(CalculateRiskLevel().ToString());
 
         public override ValueTask<string> Solve_2() => new("Answer for Part 2 will be here...");
 
-        private int CalculateRiskLevel(Dictionary<(int x, int y), int> heightmap) =>
-            FindLowestPointsOnHeightmap(heightmap).Select(height => height + 1).Sum();
+        private int CalculateRiskLevel() =>
+            FindLowestPointsOnHeightmap().Select(kvp => kvp.Value + 1).Sum();
 
-        private IEnumerable<int> FindLowestPointsOnHeightmap(Dictionary<(int x, int y), int> heightmap)
-        {
-            foreach(var heightmapEntry in heightmap)
-            {
-                if(!HasAnyAdjacentLowerPoint(heightmap, heightmapEntry.Key, heightmapEntry.Value))
-                {
-                    yield return heightmapEntry.Value;
-                }
-            }
-         }
+        private IEnumerable<KeyValuePair<(int x, int y), int>> FindLowestPointsOnHeightmap() =>
+            _heightmap.Where(entry => !HasAnyAdjacentLowerPoint(entry.Key, entry.Value));
 
-        private bool HasAnyAdjacentLowerPoint(Dictionary<(int x, int y), int> heightmap, (int x, int y) currentPosition, int currentPoint)
+        private bool HasAnyAdjacentLowerPoint((int x, int y) currentPosition, int currentPoint)
         {
             var positionUp = (currentPosition.x, currentPosition.y - 1);
-            if(IsAdjacentAtLowerPoint(heightmap, positionUp, currentPoint))
+            if(IsAdjacentAtLowerPoint(positionUp, currentPoint))
             {
                 return true;
             }
 
             var positionRight = (currentPosition.x + 1, currentPosition.y);
-            if(IsAdjacentAtLowerPoint(heightmap, positionRight, currentPoint))
+            if(IsAdjacentAtLowerPoint(positionRight, currentPoint))
             {
                 return true;
             }
 
             var positionDown = (currentPosition.x, currentPosition.y + 1);
-            if(IsAdjacentAtLowerPoint(heightmap, positionDown, currentPoint))
+            if(IsAdjacentAtLowerPoint(positionDown, currentPoint))
             {
                 return true;
             }
 
             var positionLeft = (currentPosition.x - 1, currentPosition.y);
-            if(IsAdjacentAtLowerPoint(heightmap, positionLeft, currentPoint))
+            if(IsAdjacentAtLowerPoint(positionLeft, currentPoint))
             {
                 return true;
             }
@@ -71,7 +63,7 @@ namespace AdventOfCode
             return false;
         }
 
-        private bool IsAdjacentAtLowerPoint(Dictionary<(int x, int y), int> heightmap, (int x, int y) positionToCheck, int currentPoint)
-            => heightmap.TryGetValue(positionToCheck, out var valueAtPosition) && currentPoint >= valueAtPosition;
+        private bool IsAdjacentAtLowerPoint((int x, int y) positionToCheck, int currentPoint)
+            => _heightmap.TryGetValue(positionToCheck, out var valueAtPosition) && currentPoint >= valueAtPosition;
     }
 }
