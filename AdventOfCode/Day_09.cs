410eb3115a6be4040b04ca8b47a5a804347cf7d3
Youri Bijen <youri@bijen-it.nl>
2021-12-09 17:26:37 +0100

    Day 9 - Part 1

--- AdventOfCode/Day_09.cs
+++ AdventOfCode/Day_09.cs
@@ -4,20 +4,17 @@ namespace AdventOfCode
 {
     public class Day_09 : BaseDay
     {
+        private const int TopHeight = 9;
         private readonly Dictionary<(int x, int y), int> _heightmap;
-        private readonly int MaxX;
-        private readonly int MaxY;
 
         public Day_09()
         {
             var inputLines = File.ReadAllLines(InputFilePath);
-            MaxY = inputLines.Length;
-            MaxX = inputLines[0].Length;
 
             _heightmap = new Dictionary<(int x, int y), int>();
-            for (var y = 0; y < MaxY; y++)
+            for (var y = 0; y < inputLines.Length; y++)
             {
-                for(var x = 0; x < MaxX; x++)
+                for(var x = 0; x < inputLines[0].Length; x++)
                 {
                     _heightmap.Add((x, y), int.Parse(inputLines[y][x].ToString()));
                 }
@@ -26,7 +23,56 @@ namespace AdventOfCode
 
         public override ValueTask<string> Solve_1() => new(CalculateRiskLevel().ToString());
 
-        public override ValueTask<string> Solve_2() => new("Answer for Part 2 will be here...");
+        public override ValueTask<string> Solve_2() => new(
+            GetAllBasinsBySize().OrderByDescending(s => s).Take(3).Aggregate(1, (size1, size2) => size1 * size2).ToString()
+        );
+
+        private IEnumerable<int> GetAllBasinsBySize()
+        {
+            foreach(var lowestpoint in FindLowestPointsOnHeightmap())
+            {
+                var basin = new Dictionary<(int x, int y), int>();
+                CheckAndAddToBasin(lowestpoint.Key, basin);
+                yield return basin.Count;
+            }
+        }
+
+        private void CheckAndAddToBasin((int x, int y) currentPosition, Dictionary<(int x, int y), int> currentBasin)
+        {
+            var positionUp = (currentPosition.x, currentPosition.y - 1);
+            if(IsPartOfBasin(positionUp, currentBasin))
+            {
+                currentBasin.Add(positionUp, _heightmap[positionUp]);
+                CheckAndAddToBasin(positionUp, currentBasin);
+            }
+
+            var positionRight = (currentPosition.x + 1, currentPosition.y);
+            if (IsPartOfBasin(positionRight, currentBasin))
+            {
+                currentBasin.Add(positionRight, _heightmap[positionRight]);
+                CheckAndAddToBasin(positionRight, currentBasin);
+            }
+
+            var positionDown = (currentPosition.x, currentPosition.y + 1);
+            if (IsPartOfBasin(positionDown, currentBasin))
+            {
+                currentBasin.Add(positionDown, _heightmap[positionDown]);
+                CheckAndAddToBasin(positionDown, currentBasin);
+            }
+
+            var positionLeft = (currentPosition.x - 1, currentPosition.y);
+            if (IsPartOfBasin(positionLeft, currentBasin))
+            {
+                currentBasin.Add(positionLeft, _heightmap[positionLeft]);
+                CheckAndAddToBasin(positionLeft, currentBasin);
+            }
+        }
+
+        private bool IsPartOfBasin((int x, int y) position, Dictionary<(int x, int y), int> currentBasin) =>
+            IsAdjacentPositionInHeightmap(position) && !IsAdjacentPositionAtTopHeight(position) && !currentBasin.ContainsKey(position);
+        private bool IsAdjacentPositionInHeightmap((int x, int y) positionToCheck) => _heightmap.ContainsKey(positionToCheck);
+        private bool IsAdjacentPositionAtTopHeight((int x, int y) positionToCheck) =>
+            _heightmap.TryGetValue(positionToCheck, out var height) && height == TopHeight;
 
         private int CalculateRiskLevel() =>
             FindLowestPointsOnHeightmap().Select(kvp => kvp.Value + 1).Sum();
@@ -64,6 +110,6 @@ namespace AdventOfCode
         }
 
         private bool IsAdjacentAtLowerPoint((int x, int y) positionToCheck, int currentPoint)
-            => _heightmap.TryGetValue(positionToCheck, out var valueAtPosition) && currentPoint >= valueAtPosition;
+            => _heightmap.TryGetValue(positionToCheck, out var height) && currentPoint >= height;
     }
 }
