af5b4befa71f7faff1bf7a80c3dfa41392410e5a
Youri Bijen <youri@bijen-it.nl>
2021-12-10 16:32:08 +0100

    Day 10 - Part 2

--- AdventOfCode/Day_10.cs
+++ AdventOfCode/Day_10.cs
@@ -4,14 +4,23 @@ namespace AdventOfCode
 {
     public class Day_10 : BaseDay
     {
-        private const char EmptyChar = '\0';
         private readonly List<char> _chunkOpenCharacters = new List<char> { '(', '[', '{', '<' };
+
         private readonly Dictionary<char, int> _illegalCharactersPrice = new()
         {
             { ')', 3 },
             { ']', 57 },
             { '}', 1197 },
             { '>', 25137 }
+        };
+
+        private const int AutocompleteMultiplier = 5;
+        private readonly Dictionary<char, int> _autocompleteCharacterPrice = new()
+        {
+            { ')', 1 },
+            { ']', 2 },
+            { '}', 3 },
+            { '>', 4 }
         };
 
         private readonly List<string> _navigationSubsystem;
@@ -23,7 +32,47 @@ namespace AdventOfCode
 
         public override ValueTask<string> Solve_1() => new(GetTotalSyntaxErrorScore(_navigationSubsystem).ToString());
 
-        public override ValueTask<string> Solve_2() => new("Answer for Part 2 will be here...");
+        public override ValueTask<string> Solve_2() => new(GetWinningAutocompleteScore(_navigationSubsystem).ToString());
+
+        private long GetWinningAutocompleteScore(List<string> navigationSubsystem)
+        {
+            var allScores = GetScoresForFinishingIncompleteLines(navigationSubsystem);
+            allScores.Sort();
+            return allScores[(allScores.Count / 2)];
+        }
+
+        private List<long> GetScoresForFinishingIncompleteLines(List<string> navigationSubsystem) => navigationSubsystem
+            .Select(line => ProcessLine(line))
+            .Where(result => !result.IsCorrupted)
+            .Select(result => CalculateScoreForFinishingLine(result.Line))
+            .ToList();
+
+        private long CalculateScoreForFinishingLine(string line)
+        {
+            long score = 0;
+
+            var stack = new Stack<char>();
+            foreach (var character in line)
+            {
+                if (_chunkOpenCharacters.Contains(character))
+                {
+                    stack.Push(character);
+                }
+                else
+                {
+                    stack.Pop();
+                }
+            }
+
+            while(stack.Count > 0)
+            {
+                score *= AutocompleteMultiplier;
+                var chunkCloseCharacter = GetChunkCloseCharacterForOpenCharacter(stack.Pop());
+                score += _autocompleteCharacterPrice[chunkCloseCharacter];
+            }
+
+            return score;
+        }
 
         private long GetTotalSyntaxErrorScore(List<string> navigationSubystem) =>
             GetAllCorruptedLineCharacters(navigationSubystem).Sum(c => _illegalCharactersPrice[c]);
@@ -47,6 +96,15 @@ namespace AdventOfCode
             }
             return new LineResult(line, false);
         }
+
+        private static char GetChunkCloseCharacterForOpenCharacter(char chunkOpenCharacter) => chunkOpenCharacter switch
+        {
+            '(' => ')',
+            '[' => ']',
+            '{' => '}',
+            '<' => '>',
+            _ => throw new Exception("Received unexpected ChunkOpenCharacter: " + chunkOpenCharacter)
+        };
 
         private static bool IsChunkCloseCharacter(char lastChunkOpenCharacter, char currentChar) => (lastChunkOpenCharacter == '(' && currentChar == ')')
             || (lastChunkOpenCharacter == '[' && currentChar == ']')
