d6e9b9668da24c62a04284031511111b1b23c857
Youri Bijen <youribijen@hotmail.com>
2021-12-11 19:18:05 +0100

    Commented Day 0 template

--- AdventOfCode/Day_00.cs
+++ AdventOfCode/Day_00.cs
@@ -1,18 +1,18 @@
-﻿using AoCHelper;
+﻿//using AoCHelper;
 
-namespace AdventOfCode
-{
-    public class Day_00 : BaseDay
-    {
-        private readonly string _input;
+//namespace AdventOfCode
+//{
+//    public class Day_00 : BaseDay
+//    {
+//        private readonly string _input;
 
-        public Day_00()
-        {
-            _input = File.ReadAllText(InputFilePath);
-        }
+//        public Day_00()
+//        {
+//            _input = File.ReadAllText(InputFilePath);
+//        }
 
-        public override ValueTask<string> Solve_1() => new(_input.Length.ToString());
+//        public override ValueTask<string> Solve_1() => new(_input.Length.ToString());
 
-        public override ValueTask<string> Solve_2() => new("Answer for Part 2 will be here...");
-    }
-}
+//        public override ValueTask<string> Solve_2() => new("Answer for Part 2 will be here...");
+//    }
+//}
