bc9958c61279bddf85a175b70c40afb5360e85f7
Youri Bijen <youribijen@hotmail.com>
2021-12-12 15:15:45 +0100

    Day 12 - Part 1

---
+++ AdventOfCode/Day_12.cs
@@ -0,0 +1,95 @@
+﻿using AoCHelper;
+
+namespace AdventOfCode
+{
+    public class Day_12 : BaseDay
+    {
+        private const string CaveStart = "start";
+        private const string CaveEnd = "end";
+        private readonly string _input;
+        private readonly List<Cave> _allCaves;
+
+        public Day_12()
+        {
+            _input = File.ReadAllText(InputFilePath);
+            _allCaves = GetAllCaves(_input);
+        }
+
+        public override ValueTask<string> Solve_1() => new(TraverseAllCaves(GetStartingCave(_allCaves)).Count.ToString());
+
+        public override ValueTask<string> Solve_2() => new("Answer for Part 2 will be here...");
+
+        private Cave GetStartingCave(List<Cave> allCaves) => allCaves.Find(c => c.Name == CaveStart);
+        private List<string> TraverseAllCaves(Cave startingCave)
+        {
+            var validCaveRoutes = new List<string>();
+            TraverseAllPaths(startingCave.Name + ",", startingCave.ConnectedCaves, validCaveRoutes);
+            return validCaveRoutes;
+        }
+        private void TraverseAllPaths(string currentPath, List<Cave> possibleCaves, List<string> validCaveRoutes)
+        {
+            foreach(var cave in possibleCaves)
+            {
+                if(cave.Name == CaveStart)
+                {
+                    continue;
+                }
+                else if(cave.Name == CaveEnd)
+                {
+                    validCaveRoutes.Add(currentPath + cave.Name);
+                }
+                else if(!currentPath.Contains(cave.Name))
+                {
+                    TraverseAllPaths(currentPath + cave.Name + ",", cave.ConnectedCaves, validCaveRoutes);
+                }
+                else if(IsBigCave(cave.Name))
+                {
+                    TraverseAllPaths(currentPath + cave.Name + ",", cave.ConnectedCaves, validCaveRoutes);
+                }
+            }
+        }
+
+        private bool IsBigCave(string caveName)
+        {
+            var caveCharDecimal = (int)caveName[0];
+            return caveCharDecimal >= 65 && caveCharDecimal <= 90;
+        }
+
+        private List<Cave> GetAllCaves(string input)
+        {
+            var caves = new List<Cave>();
+            foreach(var line in input.Split(Environment.NewLine))
+            {
+                var lineCaves = line.Split('-');
+                var firstCave = caves.FirstOrDefault(c => c.Name == lineCaves[0]);
+                if(firstCave == null)
+                {
+                    firstCave = new Cave(lineCaves[0]);
+                    caves.Add(firstCave);
+                }
+
+                var secondCave = caves.FirstOrDefault(c => c.Name == lineCaves[1]);
+                if(secondCave == null)
+                {
+                    secondCave = new Cave(lineCaves[1]);
+                    caves.Add(secondCave);
+                }
+
+                firstCave.ConnectedCaves.Add(secondCave);
+                secondCave.ConnectedCaves.Add(firstCave);
+            }
+            return caves;
+        }
+
+        internal class Cave
+        {
+            public Cave(string name)
+            {
+                Name = name;
+            }
+
+            public string Name { get; set; }
+            public List<Cave> ConnectedCaves { get; set; } = new List<Cave>();
+        }
+    }
+}
